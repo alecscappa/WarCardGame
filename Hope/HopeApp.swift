afeff467887b0eebf8f4e4a530b612b90e650f7c
Alec Scappa <alec.scappa@gmail.com>
2022-04-04 18:44:55 +1000

    Initial Commit

---
+++ Hope/HopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopeApp.swift
+//  Hope
+//
+//  Created by Alec Scappa on 28/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct HopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
